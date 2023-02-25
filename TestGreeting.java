32139fef4c02d647b1d586a771ee949a69d33336
MTCA52 <126246832+MTCA52@users.noreply.github.com>
2023-02-25 09:31:49 +0530

    Add files via upload

---
+++ TestGreeting.java
@@ -0,0 +1,16 @@
+public class TestGreeting
+{
+public static void main(String []args)
+{
+Greeting hello= new Greeting();
+           hello.greet();
+}
+}
+class Greeting
+{
+	void greet()
+{
+	System.out.println("hi bro");
+	System.out.println("no problem");
+}
+}
